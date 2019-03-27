3142b8281b6d23b13c4de2847b9a54cef8ab80b4
Ismail El-Habbash <gizmo-boy95@hotmail.com>
2019-03-27 22:19:06 +0800

    implements a pow algorthim for mining blocks and stores and veritifes nonce

---
+++ src/main/ProofOfWork.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
+	"strconv"
+)
+
+const targetBits = 16
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+	pow := &ProofOfWork{b, target}
+	return pow
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < math.MaxInt64 {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+	return nonce, hash[:]
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+
+	return isValid
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			intToHex(pow.block.Timestamp),
+			intToHex(int64(targetBits)),
+			intToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+func intToHex(n int64) []byte {
+	return []byte(strconv.FormatInt(n, 16))
+}
--- src/main/main.go
+++ src/main/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	bc := NewBlockchain()
@@ -11,7 +14,10 @@ func main() {
 	for i, block := range bc.blocks {
 		fmt.Printf("Prev Hash -- %x\n", block.PrevBlockHash)
 		fmt.Printf("Data -- %s\n", block.Data)
-		fmt.Printf("Hash -- %x", block.Hash)
-		fmt.Printf("Index -- %d", i)
+		fmt.Printf("Hash -- %x\n", block.Hash)
+		fmt.Printf("Index -- %d\n", i)
+
+		pow := NewProofOfWork(block)
+		fmt.Printf("POW: %s\n\n", strconv.FormatBool(pow.Validate()))
 	}
 }
